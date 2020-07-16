6574d3794dc62699226fafca0cb0141301cd41b3
Jeffrey Smith II <jeffreyssmith2nd@gmail.com>
2020-07-16 16:14:22 -0400

    Init setup

---
+++ src/main.rs
@@ -0,0 +1,12 @@
+struct Opcode {
+
+}
+
+struct Machine {
+    stack: Vec<Opcode>,
+    memory: Vec<u8>
+}
+
+fn main() {
+    println!("Hello, world!");
+}
---
+++ src/tests.rs
@@ -0,0 +1,33 @@
+mod test {
+    #[cfg(test)]
+    fn square_test() {
+        let square_code = "
+        (func $square (export "square") (type $t2) (param $p0 i32) (result i32)
+        (local $l0 i32) (local $l1 i32) (local $l2 i32) (local $l3 i32) (local $l4 i32) (local $l5 i32)
+        get_global $g0
+        set_local $l0
+        i32.const 16
+        set_local $l1
+        get_local $l0
+        get_local $l1
+        i32.sub
+        set_local $l2
+        get_local $l2
+        get_local $p0
+        i32.store offset=12
+        get_local $l2
+        i32.load offset=12
+        set_local $l3
+        get_local $l2
+        i32.load offset=12
+        set_local $l4
+        get_local $l3
+        get_local $l4
+        i32.mul
+        set_local $l5
+        get_local $l5
+        return)
+        "
+        unimplemented();
+    }
+}
